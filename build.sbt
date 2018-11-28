4cf61f33590a2c8e815791b958883dbcb2452cd8
malte-b <malte.barth@student.hpi.de>
2018-11-28 14:17:31 +0100

    add jetty launcher for easier and faster launching of web server (#3)

--- build.sbt
+++ build.sbt
@@ -17,8 +17,10 @@ libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra-json" % "2.6.3",
   "org.scalatra" %% "scalatra-swagger"  % "2.6.3",
   "org.json4s"   %% "json4s-native" % "3.5.0",
-  "ch.qos.logback" % "logback-classic" % "1.2.3" % "runtime",
-  "org.eclipse.jetty" % "jetty-webapp" % "9.4.9.v20180320" % "container",
+  "ch.qos.logback" % "logback-classic" % "1.2.3" % "runtime" exclude("org.slf4j", "slf4j-log4j12"),
+  "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container;provided",
+  "org.eclipse.jetty" % "jetty-server" % "9.2.15.v20160210" % "container;provided",
+  "org.eclipse.jetty" % "jetty-servlet" % "9.2.15.v20160210" % "container;provided",
   "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
   "com.sun.jersey" % "jersey-server" % "1.19.4",
   "net.liftweb" %% "lift-json" % "3.3.0"
---
+++ src/main/scala/com/amazon/scalatra4deequ/JettyLauncher.scala
@@ -0,0 +1,23 @@
+package com.amazon.scalatra4deequ  // remember this package in the sbt project definition
+import org.eclipse.jetty.server._
+import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
+import org.eclipse.jetty.webapp.WebAppContext
+import org.scalatra.servlet.ScalatraListener
+
+object JettyLauncher { // this is my entry object as specified in sbt project definition
+  def main(args: Array[String]) {
+    val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
+
+    val server = new Server(port)
+    val context = new WebAppContext()
+    context setContextPath "/"
+    context.setResourceBase("src/main/scala/com/amazon/scalatra4deequ")
+    context.addEventListener(new ScalatraListener)
+    context.addServlet(classOf[DefaultServlet], "/")
+
+    server.setHandler(context)
+
+    server.start
+    server.join
+  }
+}
